0a98d061f15743a237bf4ef632d9b9761e876776
ludics <ludi7319@163.com>
2019-08-26 21:14:50 +0800

    update gitalk

--- dist/main.js
+++ dist/main.js
@@ -2,11 +2,11 @@ const pathname = window.location.pathname;
 
 if (pathname.endsWith('html') && pathname.indexOf("/hidden/") === -1) {
   const gitalk = new Gitalk({
-    clientID: '49aeab30501e804c71b0',
-    clientSecret: '6d3c8320ac0c5c461afc2c01994833f05056b0de',
-    repo: 'sf-zhou.github.io',
-    owner: 'SF-Zhou',
-    admin: ['SF-Zhou'],
+    clientID: '1bf57da7ef29fff1c805',
+    clientSecret: '4047aa37daa86e166357b057d7e0417e6ed1fd3a',
+    repo: 'ludics.github.io',
+    owner: 'ludics',
+    admin: ['ludics'],
     id: pathname,
   });
 
