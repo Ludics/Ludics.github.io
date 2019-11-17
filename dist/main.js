fc0ecbc78dbcd0c433da331fc53bb7311a1c79a0
ludics <ludi7319@163.com>
2019-11-18 01:36:29 +0800

    update with sf-zhou

--- dist/main.js
+++ dist/main.js
@@ -7,10 +7,34 @@ if (pathname.endsWith('html') && pathname.indexOf("/hidden/") === -1) {
     repo: 'ludics.github.io',
     owner: 'ludics',
     admin: ['ludics'],
-    id: md5(pathname),
+    id: pathname,
   });
-
   gitalk.render('gitalk-container');
+}
+
+if (pathname === "/" || pathname === "/index.html") {
+  window.onhashchange = function () {
+    const mark = window.location.hash.substr(2);
+    const cards = document.getElementsByClassName("card");
+    for (card of cards) {
+      var found = false;
+      const tags = card.getElementsByClassName("tag");
+      for (tag of tags) {
+        const a = tag.children[0];
+        if (a.innerText === mark) {
+          tag.classList.add("selected");
+          a.href = "#";
+          found = true;
+        } else {
+          tag.classList.remove("selected");
+          a.href = "#/" + a.innerText;
+        }
+      }
+      card.hidden = mark.length > 0 && !found;
+    }
+  }
+
+  window.onhashchange();
 }
 
 // Google Analytics
